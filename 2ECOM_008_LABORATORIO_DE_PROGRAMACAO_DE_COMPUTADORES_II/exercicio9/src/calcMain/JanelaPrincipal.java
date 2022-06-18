9d63b12633a0cbba2f9efb24b7d230bbd44bb484
Lucas <76121669+lsrdot@users.noreply.github.com>
2022-06-18 13:13:23 -0300

    Update JanelaPrincipal.java
    Excluindo informações não essenciais (dados de matrícula)

--- 2ECOM_008_LABORATORIO_DE_PROGRAMACAO_DE_COMPUTADORES_II/exercicio9/src/calcMain/JanelaPrincipal.java
+++ 2ECOM_008_LABORATORIO_DE_PROGRAMACAO_DE_COMPUTADORES_II/exercicio9/src/calcMain/JanelaPrincipal.java
@@ -7,7 +7,6 @@ package calcMain;
  * @author Pedro costa calazans. https://github.com/pedrocostacalazans
  * @author Pablo Vasconcelos da Cruz. https://github.com/Pablo321123
  * @version 1.0
- * Matriculas: 20203018919, 20203018697, 20203018801.
  */
 
 import javafx.application.Application;
