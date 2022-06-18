a38e514fc9b57629b6550c8087e9614c8cf0ddd4
Lucas <76121669+lsrdot@users.noreply.github.com>
2022-06-18 13:09:49 -0300

    Update Home.java
    Excluindo informações não essenciais (dados de matrícula)

--- 2ECOM_008_LABORATORIO_DE_PROGRAMACAO_DE_COMPUTADORES_II/exercicio7/jogo-da-velha/src/br/cefetmg/Home.java
+++ 2ECOM_008_LABORATORIO_DE_PROGRAMACAO_DE_COMPUTADORES_II/exercicio7/jogo-da-velha/src/br/cefetmg/Home.java
@@ -8,12 +8,11 @@ import java.util.Locale;
 
 /**
  * CENTRO FEDERAL DE EDUCACAO TECNOLOGICA DE MINAS GERAIS (CEFET-MG).
- * DISCIPLICA: 2ECOM.017 - LABORATORIO DE SISTEMAS DIGITAIS PARA COMPUTACAO - T01 (2021.1 - 5T34).
+ * DISCIPLINA: 2ECOM.017 - LABORATORIO DE SISTEMAS DIGITAIS PARA COMPUTACAO - T01 (2021.1 - 5T34).
  *
  * @author Lucas Siqueira Ribeiro - https://github.com/lucasdot
  * @author Pedro Costa Calazans - https://github.com/pedrocostacalazans
  *
- * Matriculas: 20203018919, 20203018697.
  *
  * @version 1.0
  */
