d391b87708f4c60c3daee7cb0d72fe6df55bae38
Lucas <76121669+lsrdot@users.noreply.github.com>
2022-06-18 13:08:24 -0300

    Update Enigma.java
    Excluindo informações não essenciais (dados de matrícula)

--- 2ECOM_008_LABORATORIO_DE_PROGRAMACAO_DE_COMPUTADORES_II/exercicio6/src/br/cefetmg/Enigma.java
+++ 2ECOM_008_LABORATORIO_DE_PROGRAMACAO_DE_COMPUTADORES_II/exercicio6/src/br/cefetmg/Enigma.java
@@ -10,7 +10,6 @@ import java.util.Scanner;
  * @author Pedro costa calazans. https://github.com/pedrocostacalazans
  * @author Pablo Vasconcelos da Cruz. https://github.com/Pablo321123
  * @version 1.0
- * Matriculas: 20203018919, 20203018697, 20203018801.
  */
 
 public class Enigma {
