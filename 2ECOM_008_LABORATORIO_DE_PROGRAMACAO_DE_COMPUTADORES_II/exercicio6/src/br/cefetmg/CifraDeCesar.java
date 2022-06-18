066e8e89554a5e0c69893d7a9f0ea77d05ea1af8
Lucas <76121669+lsrdot@users.noreply.github.com>
2022-06-18 13:07:43 -0300

    Update CifraDeCesar.java
    Excluindo informações não essenciais (dados de matrícula)

--- 2ECOM_008_LABORATORIO_DE_PROGRAMACAO_DE_COMPUTADORES_II/exercicio6/src/br/cefetmg/CifraDeCesar.java
+++ 2ECOM_008_LABORATORIO_DE_PROGRAMACAO_DE_COMPUTADORES_II/exercicio6/src/br/cefetmg/CifraDeCesar.java
@@ -4,10 +4,9 @@ package br.cefetmg;
  * 2ECOM.008 - LABORATÓRIO DE PROGRAMAÇÃO DE COMPUTADORES II - T05 (2021.1 -
  * 5T12) - CEFET-MG.
  *
- * @author Lucas Siqueira Ribeiro. https://github.com/lucasdot
+ * @author Lucas Siqueira Ribeiro. https://github.com/lsrdot
  * @author Pedro costa calazans. https://github.com/pedrocostacalazans
  * @author Pablo Vasconcelos da Cruz. https://github.com/Pablo321123
- * @version 1.0 Matriculas: 20203018919, 20203018697, 20203018801.
  */
 
 public class CifraDeCesar {
